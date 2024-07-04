3a94165cc4d3988d4a9e9d87a9aed7913d0d22d2
Sedly <sedly.keith@student.moringaschool.com>
2024-07-04 09:35:36 +0300

    python-p4-validation-flask-sqlalchemy-validations- lab

--- server/models.py
+++ server/models.py
@@ -13,6 +13,24 @@ class Author(db.Model):
 
     # Add validators 
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('name is required')
+        author = db.session.query(Author).filter_by(name=name).first()
+        if author is not None:
+            raise ValueError('name must be unique')
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number:
+            raise ValueError('phone number is required')
+        if len(phone_number) != 10:
+            raise ValueError('phone number must be 10 digits')
+        return phone_number
+    
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,7 +45,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+
+    @validates('title') 
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title field is required.")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if( key == 'content'):
+            if len(string) < 250:
+                raise ValueError("Post content must be greater than or equal 250 characters long.")
+        if( key == 'summary'):
+            if len(string) > 250:
+                raise ValueError("Post summary must be less than or equal to 250 characters long.")
+        return string
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category
 
 
     def __repr__(self):
